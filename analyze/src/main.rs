49c21fb60dceb193e26021a88e9c6b1a9638d5dd
Moritz Hoffmann <antiguru@gmail.com>
2021-12-27 14:06:28 +0100

    Simplify activation in analysis
    Signed-off-by: Moritz Hoffmann <antiguru@gmail.com>

--- analyze/src/main.rs
+++ analyze/src/main.rs
@@ -1,5 +1,4 @@
 use std::net::TcpListener;
-use std::time::Duration;
 
 use crossbeam_channel::{bounded, TryRecvError};
 
@@ -11,40 +10,42 @@ use track_types::{TraceProtocol, ENV_HEAP_ANALYSIS_ADDR};
 
 fn main() {
     timely::execute_from_args(std::env::args(), |worker| {
-        let mut conn = if worker.index() == 0 {
-            let (sender, receiver) = bounded(64);
-
-            std::thread::spawn(move || {
-                let addr = std::env::var(ENV_HEAP_ANALYSIS_ADDR);
-                let addr = addr.as_deref().unwrap_or("localhost:64123");
-                let listener = TcpListener::bind(addr).unwrap();
-                let stream = listener.incoming().next().unwrap().unwrap();
-                loop {
-                    match bincode::deserialize_from::<_, TraceProtocol>(&stream) {
-                        Ok(data) => sender.send(data).unwrap(),
-                        Err(err) => {
-                            eprintln!("Exiting reader thread: {:?}", err);
-                            break;
-                        }
-                    }
-                }
-            });
-            Some(receiver)
-        } else {
-            None
-        };
-
-        worker.dataflow::<u64, _, _>(move |scope| {
+        worker.dataflow::<u64, _, _>(|scope| {
+            let index = scope.index();
             source(scope, "Trace reader", |cap, info| {
-                let activator = scope.activator_for(&info.address[..]);
-                let mut conn = conn.take();
+                let activator = scope.sync_activator_for(&info.address[..]);
+                let mut conn = if index == 0 {
+                    let (sender, receiver) = bounded(64);
+
+                    std::thread::spawn(move || {
+                        let addr = std::env::var(ENV_HEAP_ANALYSIS_ADDR);
+                        let addr = addr.as_deref().unwrap_or("localhost:64123");
+                        let listener = TcpListener::bind(addr).unwrap();
+                        let stream = listener.incoming().next().unwrap().unwrap();
+                        loop {
+                            match bincode::deserialize_from::<_, TraceProtocol>(&stream) {
+                                Ok(data) => {
+                                    sender.send(data).unwrap();
+                                    activator.activate().unwrap();
+                                },
+                                Err(err) => {
+                                    eprintln!("Exiting reader thread: {:?}", err);
+                                    break;
+                                }
+                            }
+                        }
+                    });
+                    Some(receiver)
+                } else {
+                    None
+                };
                 let mut cap = Some(cap);
                 move |output| {
-                    let (exit, activate) = if let Some(cap) = cap.as_mut() {
+                    let exit = if let Some(cap) = cap.as_mut() {
                         if let Some(conn) = &conn {
                             let mut fuel = 16;
-                            let mut res = (true, false);
-                            while fuel > 0 && res.0 {
+                            let mut res = false;
+                            while fuel > 0 && !res {
                                 fuel -= 1;
                                 res = match conn.try_recv() {
                                     Ok(TraceProtocol::Instructions {
@@ -56,30 +57,29 @@ fn main() {
                                             .map(|(data, time)| ((thread_id, data), time))
                                             .collect();
                                         output.session(&cap).give_vec(&mut data);
-                                        (false, true)
+                                        false
                                     }
                                     Ok(TraceProtocol::Timestamp(timestamp)) => {
                                         cap.downgrade(&timestamp);
-                                        (false, true)
+                                        false
                                     }
-                                    Err(TryRecvError::Disconnected) => (true, false),
-                                    Err(TryRecvError::Empty) => (false, false),
+                                    Err(TryRecvError::Disconnected) => true,
+                                    Err(TryRecvError::Empty) => {
+                                        fuel = 0;
+                                        false
+                                    },
                                 }
                             }
                             res
                         } else {
-                            (true, false)
+                            true
                         }
                     } else {
-                        (true, false)
+                        true
                     };
                     if exit {
                         cap.take();
                         conn.take();
-                    } else if activate {
-                        activator.activate();
-                    } else {
-                        activator.activate_after(Duration::from_millis(100));
                     }
                 }
             })
